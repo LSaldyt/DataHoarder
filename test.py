796b32c80de62fbb67ba81c6736ea42deeee1fc0
LSaldyt <lucassaldyt@yahoo.com>
2016-07-23 01:09:20 -0600

    Change test.py to stream submissions and serialize
    Is still somewhat inefficient. (Produces a whole bunch of usable data, though)

--- test.py
+++ test.py
@@ -1,25 +1,35 @@
 #!/usr/bin/env python3
 import praw
+from praw.errors import HTTPException, APIException
 import time
+import pickle
 
-from pprint import pprint
-r = praw.Reddit('Simple reddit crawler by /u/bearded_vagina. Contact: lucassaldyt@gmail.com. Application is at github.com/LSaldyt/ (GPL)')
+start   = time.time()
+current = start
+i = 0
 
+while True:
+    try:
+        r = praw.Reddit('Simple reddit crawler by /u/bearded_vagina. Contact: lucassaldyt@gmail.com. Application is at github.com/LSaldyt/ (GPL)')
+        for submission in praw.helpers.submission_stream(r, 'all'):
+            i += 1
 
-all_comments = r.get_comments('all', limit=10)
+            print('Opened submission %s' % submission.title)
 
-nice_format = \
-'''
-%s/%s (%s):
-%s
-'''
+            comments = []
+            for comment in submission.comments:
+                comments.append((comment.author, comment.body))
+                for subcomment in comment.replies:
+                    comments.append((comment.author, comment.body))
+            with open('serialized/%s.pkl' % i, 'wb') as picklefile:
+                pickle.dump((submission.author, 
+                             submission.title + submission.selftext, 
+                             comments), picklefile)
+            taken   = time.time() - current # "Current"
+            current = time.time()
+            print('Wrote submission. Total elapsed: %s. For submission serialization: %s' % (current - start, taken))
 
-for c in all_comments:
-    comment = (nice_format %( 
-            c.subreddit,
-            c.author,
-            c.score,
-            c.body)
-            )
-    print(comment)
-    time.sleep(1)
+    except HTTPException or APIException as e: # Catch what isn't my fault
+        print(e)
+        time.sleep(1)
+
