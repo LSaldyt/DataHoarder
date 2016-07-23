ae9ee49d814633a4f07d5bedf850ec1c3b9fc0bd
LSaldyt <lucassaldyt@yahoo.com>
2016-07-22 22:22:55 -0600

    Add initial script

---
+++ test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import praw
+r = praw.Reddit(user_agent='my_cool_application')
+submissions = r.get_subreddit('opensource').get_hot(limit=5)
+[print(str(x)) for x in submissions]
+
