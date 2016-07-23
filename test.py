4b247b7a6da5a639bd5b6710430d1b87055801d5
LSaldyt <lucassaldyt@yahoo.com>
2016-07-23 00:16:33 -0600

    Adds test.py, which prints recent reddit comments

--- test.py
+++ test.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python3
 import praw
-r = praw.Reddit(user_agent='my_cool_application')
-submissions = r.get_subreddit('opensource').get_hot(limit=5)
-[print(str(x)) for x in submissions]
+import time
 
+from pprint import pprint
+r = praw.Reddit('Simple reddit crawler by /u/bearded_vagina. Contact: lucassaldyt@gmail.com. Application is at github.com/LSaldyt/ (GPL)')
+
+
+all_comments = r.get_comments('all', limit=10)
+
+nice_format = \
+'''
+%s/%s (%s):
+%s
+'''
+
+for c in all_comments:
+    comment = (nice_format %( 
+            c.subreddit,
+            c.author,
+            c.score,
+            c.body)
+            )
+    print(comment)
+    time.sleep(1)
