3a29cb44ee07312ec3b07e9ede200562e1549ad7
LSaldyt <lucassaldyt@yahoo.com>
2016-07-24 16:07:48 -0600

    Deletes duplicate code/improves crawling
    Adds crawling templates and fixes a bug in wikipedia crawling (missing
    call to .split())

--- src/fetch.py
+++ src/fetch.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 from settings      import submission_attrs, comment_attrs, group_size
 from serialization import serialize, concatenate
+from contextlib    import contextmanager
 
-import time
+import time, sys
 
 # Helper function for taking attributes from readable reddit objects
 def collect_attrs(item, attributes):
@@ -18,6 +19,8 @@ def make_log(silent):
         if not silent:
             print(*args, **kwargs)
     return log
+
+
 
 # Indefinitely read stuff off of reddit
 def fetch_reddit(silent=False):
@@ -83,80 +86,75 @@ def fetch_gutenberg():
             concatenate('guten')
             lastsave = time.time()
 
-def fetch_google(begin='spiders', save_interval=10): # Start crawling from here
+@contextmanager
+def redirect(filename):
+    with open(filename, 'w') as output:
+        sys.stdout = output
+        try:
+            yield
+        finally:
+            sys.stdout = sys.__stdout__
+
+
+def fetch_template(crawl):
+    def template(remaining):
+        seen = set()
+        while True:
+            next_remaining = set()
+            for item in remaining:
+                next_remaining.update(crawl(item, seen))
+            remaining = next_remaining
+    return template
+
+
+def crawl_template(source, get, exceptions, silent=True, interval=10, defaultval=set()):
+    def template(item, seen):
+        print('Crawling %s' % item)
+        if int(time.time()) % interval == 0:
+            print('Saving')
+            concatenate(source)
+        try:
+            seen.add(item)
+            content, remaining = get(item, seen)
+            serialize(content, '../serialized/%s%s' % (source, len(seen)))
+            return remaining 
+        # If something goes wrong (on API side), throw away everything from this section of the crawl
+        except exceptions as e: 
+            if not silent:
+                print(repr(e))
+            return defaultval
+        except KeyboardInterrupt:
+            concatenate(source)
+            sys.exit(0)
+    return template
+
+
+def get_google(url, seen):
     from bs4    import BeautifulSoup
     from google import search, get_page
-    from urllib.error import HTTPError, URLError, ContentTooShortError
+    soup      = BeautifulSoup(get_page(url), 'lxml')
+    content   = { url : [str(s) for paragraph in soup.find_all('p') for s in paragraph.strings] }
+    remaining = { link.get('href') for link in soup.find_all('a') if link.get('href') not in seen }
+    return content, remaining
 
-    searched  = set()
-    remaining = set()
-    content   = dict()
+from urllib.error import HTTPError, URLError, ContentTooShortError
+crawl_google = crawl_template('google', get_google, (HTTPError, URLError, ContentTooShortError, ValueError))
+fetch_google = fetch_template(crawl_google)
 
-    def crawl(page):
-        print('Crawling %s' % page)
-        if int(time.time()) % save_interval == 0:
-            print('Saving')
-            concatenate('google')
-
-        try:
-            searched.add(page)
-            soup = BeautifulSoup(get_page(page), 'lxml')
-            for paragraph in soup.find_all('p'):
-                content[page] = [str(s) for s in paragraph.strings]
-            serialize(content, '../serialized/google%s' % len(searched))
-            content.clear()
-        # If something goes wrong (on API side), throw away everything from this section of the crawl
-        except HTTPError or URLError or ContentTooShortError as e: 
-            print(e)
-            content.clear()
-            return [] # There will be plenty of other links, hopefully!
-        except Exception as e:
-            print('Unknown exception occurred, but we want to continue anyways, right?')
-            print(e)
-            content.clear()
-            return []
-
-        return [link.get('href') for link in soup.find_all('a')]
-
-    remaining = search(begin)
-
-    while True:
-        next_remaining = set()
-        for page in remaining:
-            next_remaining.update(set(crawl(page)))
-        remaining = next_remaining
-
-def fetch_wikipedia(begin='spiders', save_interval=10):
+def get_wikipedia(term, seen):
     import wikipedia
-    from wikipedia.exceptions import WikipediaException
-    content = dict()
-    seen    = set()
+    content   = dict()
+    remaining = set()
+    for item in wikipedia.search(term, results=10):
+        if item not in seen:
+            content[item] = wikipedia.summary(item)
+        remaining = { word for summary in content.values() for word in summary.split() if word not in seen}
+    return content, remaining
 
-    def crawl(term):
-        print('Crawling for %s' % term)
-        seen.add(term)
-        to_lookup = set()
-        try:
-            for item in wikipedia.search(term, results=10):
-                print('Getting summary on %s' % item)
-                info          = wikipedia.summary(item)
-                content[item] = info
-                to_lookup.update(set([word for word in info if word not in seen]))
-            serialize(content, '../serialized/wiki%s' % len(seen))
-            if int(time.time()) % save_interval == 0:
-                print('Saving')
-                concatenate('wiki')
-        except WikipediaException as e:
-            print(e)
-        return to_lookup
-
-    remaining = crawl(begin)
-    while True:
-        next_remaining = set()
-        for item in remaining:
-            next_remaining.update(crawl(item))
-        remaining = next_remaining
-        
+from wikipedia.exceptions import WikipediaException
+crawl_wikipedia = crawl_template('wiki', get_wikipedia, (WikipediaException,))
+fetch_wikipedia = fetch_template(crawl_wikipedia)
 
 if __name__ == '__main__':
-    fetch_wikipedia()
+    fetch_google({'https://en.wikipedia.org/wiki/Spider'})
+    fetch_wikipedia({'spiders'})
